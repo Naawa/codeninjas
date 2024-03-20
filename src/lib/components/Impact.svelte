cc8bfcb00808cfc10f380a0f28b36f535cfdebc5
Naous Islam <naous.islam@outlook.com>
2024-03-19 19:16:48 -0700

    Added another video.

---
+++ src/lib/components/Impact.svelte
@@ -0,0 +1,22 @@
+<section>
+    <iframe src="https://www.youtube.com/embed/-N08PV3Cs6Q?vq=hd1080" title="Introducing Code Ninjas&#39;s IMPACT educational platform" frameborder="0" allowfullscreen></iframe>
+</section>
+
+<style lang="scss">
+    section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        iframe {
+            aspect-ratio: 16/9;
+            min-height: 400px;
+            max-height: 60vh;
+            min-width: 300px;
+            max-width: 90vw;
+            pointer-events: all;
+            border-radius: 1em;
+            box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.39);
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 	import Junior from "$lib/components/Junior.svelte";
 	import Camps from "$lib/components/Camps.svelte";
 	import OtherPrograms from "$lib/components/OtherPrograms.svelte";
+	import Impact from "$lib/components/Impact.svelte";
 	export let data;
 </script>
 
@@ -19,6 +20,7 @@
 <Junior></Junior>
 <Camps></Camps>
 <OtherPrograms></OtherPrograms>
+<Impact></Impact>
 <LocationsHeading></LocationsHeading>
 <LocationMap></LocationMap>
 <InquiryForm {data}></InquiryForm>
